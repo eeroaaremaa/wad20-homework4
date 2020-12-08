109e51b0c121f6644ca6cdd6e683b073f1f9e076
eeroaaremaa <eero.aaremaa@gmail.com>
2020-12-08 19:50:55 +0200

    Added like functionality

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -25,7 +25,6 @@ router.get('/', authorize, (request, response) => {
 router.post('/', authorize,  (request, response) => {
 
     // Endpoint to create a new post
-    console.log("tessdft" );
     let media = {
         url: request.body.media.url,
         type: request.body.media.type
@@ -61,12 +60,21 @@ router.post('/', authorize,  (request, response) => {
 router.put('/:postId/likes', authorize, (request, response) => {
 
     // Endpoint for current user to like a post
+    let userId = request.currentUser.id;
+
+    PostModel.like(userId, request.params.postId, () => {
+        response.status(201).json()
+    });
 });
 
 router.delete('/:postId/likes', authorize, (request, response) => {
 
     // Endpoint for current user to unlike a post
+    let userId = request.currentUser.id;
 
+    PostModel.unlike(userId, request.params.postId, () => {
+        response.status(201).json()
+    });
 });
 
 module.exports = router;
